863c89926e0572ce5c95137acd83c88999668d96
asmaulhusnamayad <113965594+asmaulhusnamayad@users.noreply.github.com>
2022-10-11 13:45:17 +0600

    Add files via upload

---
+++ lab3_prblm3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int studentID[3][3];
+    int completeCredits;
+    double CGPA;
+    for(int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+        cin>>studentID[i][j];
+    }
+}
+    for(int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+        cout<<studentID[i][j];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
